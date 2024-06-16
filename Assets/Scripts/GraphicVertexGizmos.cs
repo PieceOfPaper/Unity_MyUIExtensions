b3efb059b7ee8989b7521f20fe086448396c07ee
이성희 <the.paper@live.co.kr>
2024-06-16 16:53:55 +0900

    GraphicVertexGizmos 추가

---
+++ Assets/Scripts/GraphicVertexGizmos.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteAlways]
+public class GraphicVertexGizmos : BaseMeshEffect
+{
+    public Color gizmosColor = Color.green;
+    
+    private readonly List<UIVertex> m_Vertices = new List<UIVertex>();
+    
+    public override void ModifyMesh(VertexHelper vh)
+    {
+        m_Vertices.Clear();
+        vh.GetUIVertexStream(m_Vertices);
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.color = gizmosColor;
+        for (var verIdx = 0; verIdx < m_Vertices.Count; verIdx ++)
+        {
+            var v = m_Vertices[verIdx].position;
+            Gizmos.DrawSphere(v, 2.0f);
+        }
+        var triCnt = m_Vertices.Count / 3;
+        for (var triIdx = 0; triIdx < triCnt; triIdx ++)
+        {
+            var v0 = m_Vertices[triIdx * 3 + 0].position;
+            var v1 = m_Vertices[triIdx * 3 + 1].position;
+            var v2 = m_Vertices[triIdx * 3 + 2].position;
+            Gizmos.DrawLine(v0, v1);
+            Gizmos.DrawLine(v1, v2);
+            Gizmos.DrawLine(v2, v0);
+        }
+    }
+#endif
+}
